b6d1377f67059ad65069d3af19e6ed31f3b61f95
MundakaZgz <j.herrero.moriana@gmail.com>
2022-06-01 16:08:29 +0200

    Add main page

---
+++ main.mk
@@ -0,0 +1,54 @@
+# IT Manager Skills Training
+
+In this document we will explore the areas of expertise all IT managers should have and the available trainings that will help them to become more effective.
+
+## Areas
+Every manager is responsible for a number of areas. These areas are:
+- [ ] People management
+- [ ] IT Project Management
+
+<!--
+- [ ] IT Infrastructure
+- [ ] IT Security
+- [ ] IT Management
+- [ ] IT Operations
+- [ ] IT Support
+- [ ] IT Quality
+- [ ] IT Change Management
+- [ ] IT Project Management
+- [ ] IT Risk Management
+-->
+
+Gestión de equipos.
+Control de los sistemas informáticos.
+Planificación y desarrollo de proyectos de potenciación de sistemas informáticos.
+El desarrollo de nuevos proyectos.
+Planificación de la configuración de nuevas máquinas y software.
+Verificar la correcta gestión de la base de datos de la empresa.
+Desarrollar estrategias de ciberseguridad adecuadas.
+Verificar el correcto funcionamiento del software de seguridad.
+Elaboración de directrices, protocolos y procedimientos funcionales.
+Gestionar el presupuesto asignado al área técnica.
+Identificar y gestionar las nuevas necesidades empresariales que implican o podrían implicar a las TI.
+Colaboración constante con el resto de la dirección. 
+
+
+## Trainings
+### People management skills
+### Self improvement
+### IT Skills
+### Financial skills
+### Communication skills
+### Teamwork
+### Leadership
+### Risk management
+### Change management
+### IT Security
+### IT Quality
+### IT Project Management
+### IT Infrastructure
+### IT Management
+### IT Operations
+### IT Support
+
+
